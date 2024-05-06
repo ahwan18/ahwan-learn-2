1bf00d704be24d00ce2aa9c696ea5a831dcf5bb1
ahwan18 <ahmadkurniawanibrahim@gmail.com>
2024-05-06 09:16:14 +0700

    Modifying config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@
 import { initializeApp, getApps } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyB-YCG2ICSHwCElnJLqnZRZ-Qz9KY0BFjY",
+    authDomain: "ahwan-learn-2.firebaseapp.com",
+    projectId: "ahwan-learn-2",
+    storageBucket: "ahwan-learn-2.appspot.com",
+    messagingSenderId: "1085389160462",
+    appId: "1:1085389160462:web:536ea812a09a750bcf3c5f",
+    measurementId: "G-KKBQMB4F63"
 };
 
 // Initialize Firebase
